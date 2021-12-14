1c5fe26d69e41145797522a30f7b4b5e7eebe4ba
manuelcely <manuelcely75@gmail.com>
2021-12-14 07:57:48 -0500

    first commit

---
+++ petApp/serializers/petSerializer.py
@@ -0,0 +1,9 @@
+from petApp.models.pet import Pet
+from rest_framework import serializers
+
+
+class PetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pet
+        fields = ['id_pet', 'name', 'specie', 'breed', 'gender', 'bday_aprox',
+                  'date_register', 'description', 'image', 'avaliable', 'user']
---
+++ petApp/views/__init__.py
@@ -0,0 +1,1 @@
+from .petView import petAPI
